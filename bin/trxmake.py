9c3393db5d235e686d9004f4fe0f7835693152b8
Hélder Rodrígues <helder0xff@gmail.com>
2021-04-12 22:46:08 +0200

    FEATURE: C_ModuleGenerator implemented.

---
+++ bin/trxmake.py
@@ -0,0 +1,108 @@
+import os
+from datetime import date
+
+class C_ModuleGenerator:
+	def __init__( self, modName = None, parentFolder = '.' ):
+		self.modName = modName
+		self.parentFolder = parentFolder
+		self.dirPath = self.parentFolder + '/' + self.modName + '/'
+
+	def GenerateModule( self ):
+		self._MakeDirs( )
+		self._WriteTemplates( )
+		self._GenerateModuleTest( )
+
+	def _GenerateModuleTest( self ):
+		modName = self.modName
+		parentFolder = self.parentFolder
+		dirPath = self.dirPath
+
+		self.modName = "test"
+		self.parentFolder = parentFolder + "/" + modName
+		self.dirPath = self.parentFolder + '/' + self.modName + '/'
+
+		self._MakeDirs( )
+		self._WriteTemplates( )
+
+		self.modName = modName
+		self.parentFolder = parentFolder
+		self.dirPath = dirPath
+
+	def _MakeDirs( self ):
+		os.system( "mkdir -p " + self.dirPath )
+		os.system( "mkdir -p " + self.dirPath + "/src" )
+		os.system( "mkdir -p " + self.dirPath + "/inc" )
+		os.system( "mkdir -p " + self.dirPath + "/build" )
+
+	def _WriteTemplates( self ):
+		template = self._GenerateHeaderTemplate( )
+		file = open( self.dirPath + "/inc/%s.h"%( self.modName ), "w" )
+		file.write( template )
+		file.close( )
+
+		template = self._GenerateSourceTemplate( )
+		file = open( self.dirPath + "/src/%s.c"%( self.modName ), "w" )
+		file.write( template )
+		file.close( )		
+
+	def _GenerateHeaderTemplate( self ):
+		template = 							\
+			"""/**							\
+			\n * @file\t%s.h				\
+			\n * @author\t%s				\
+			\n * @date\t%s					\
+			\n * @brief\t??					\
+			\n */							\
+			\n 								\
+			\n#ifndef %s_H					\
+			\n#define %s_H 					\
+			\n								\
+			\n#endif /* %s_H */				\
+			\n								\
+			\n//*** end of file ***//		\
+			\n"""							\
+			%( 	self.modName, 				\
+				os.environ[ 'USER' ],		\
+				date.today(), 				\
+				self.modName.upper( ), 		\
+				self.modName.upper( ), 		\
+				self.modName.upper( ) )
+
+		return template
+
+	def _GenerateSourceTemplate( self ):
+		template = 													\
+			"""/**													\
+			\n * @file\t%s.c										\
+			\n * @author\t%s										\
+			\n * @date\t%s											\
+			\n * @brief\t??											\
+			\n */													\
+			\n 														\
+			\n/* Includes go here.	*/								\
+			\n#include <%s.h>										\
+			\n														\
+			\n/* typedefs go here.	*/								\
+			\n 														\
+			\n/* Consts go here.	*/								\
+			\n 														\
+			\n/* #defines go here.	*/								\
+			\n 														\
+			\n/* static vars go here.	*/							\
+			\n														\
+			\n/* static function declarations go here.	*/			\
+			\n 														\
+			\n/* non static function implementation go here.	*/	\
+			\n 														\
+			\n/* static function implementation go here.	*/		\
+			\n 														\
+			\n//*** end of file ***//								\
+			\n"""													\
+			%( 	self.modName, 										\
+				os.environ[ 'USER' ],								\
+				date.today(), 										\
+				self.modName )
+
+		return template		
+
+### end of file ###
---
+++ test/test_trxmake.py
@@ -0,0 +1,49 @@
+import os, sys, glob
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+
+sys.path.append(parentdir)
+from bin import trxmake
+
+PASS = 0
+FAIL = -1
+
+def main( ):
+	result = PASS
+
+	result |= _testModuleGeneration( )
+
+	if result:
+		print("FAIL")
+	else:
+		print("PASS")
+
+	return result
+
+def _testModuleGeneration( ):
+	module = "mod1"
+	os.system( "rm -rf ./%s"%( module ) )
+
+	modGen = trxmake.C_ModuleGenerator( modName = module, parentFolder = "./" )
+	modGen.GenerateModule( )
+
+	expectedList = ['./mod1/',												\
+					'./mod1/src', './mod1/src/mod1.c',						\
+					'./mod1/build',											\
+					'./mod1/inc', './mod1/inc/mod1.h',						\
+					'./mod1/test',											\
+					'./mod1/test/src', './mod1/test/src/test.c', 			\
+					'./mod1/test/build',									\
+					'./mod1/test/inc', './mod1/test/inc/test.h']
+
+	obtainedList = glob.glob( "./" + module + "/**", recursive = True )
+	os.system( "rm -rf ./%s"%( module ) )
+
+	result = FAIL
+	if ( expectedList == obtainedList ):
+		result = PASS
+
+	return result
+
+main( )
+
